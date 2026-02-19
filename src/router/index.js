4728e7175fefd1811ec50b881e488bc2b571eabe
Yersultan <ye.zhetibayev@gmail.com>
2026-02-19 23:11:11 +0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router"
+import DashboardView from "@/views/DashboardView.vue"
+import LoginView from "@/views/LoginView.vue"
+
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/login'
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginView
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardView,
+        meta: { requiresAuth: true}
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('token')
+    if(to.meta.requiresAuth && !token) {
+        next('/login')
+    } else {
+        next()
+    }
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>Login</div>
+    <p>hello</p>
+    </template>
