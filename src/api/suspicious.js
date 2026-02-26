9aea3da7723a2f4a10c1a2e53dcc519933351c9d
Yersultan <ye.zhetibayev@gmail.com>
2026-02-26 23:05:36 +0500

    resolve the issue

--- src/api/suspicious.js
+++ src/api/suspicious.js
@@ -1,11 +1,9 @@
 import api from "./axios";
 
 export const getSuspiciousPackets = (params = {}) => {
-    return api.get('suspicious/packets,', { params })
+  return api.get('/suspicious/packets', { params })
 }
 
 export const updatePacketLabel = (packetId, label) => {
-    return api.patch('suspicious/packets/${packetId}/label', { label })
+  return api.patch(`/suspicious/packets/${packetId}/label`, { label })
 }
-
-
